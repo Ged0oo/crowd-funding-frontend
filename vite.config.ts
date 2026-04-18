e3232e0fa407515d375fba9bc202c433700bc798
Ged0oo <mohmmednagy@gmail.com>
2026-04-18 22:57:01 +0200

    solve on vercel production bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     // Add these headers to allow the Google OAuth popup to communicate with your app
     headers: {
